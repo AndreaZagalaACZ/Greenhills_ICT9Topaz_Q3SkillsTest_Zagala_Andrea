17674a81813ec577a24b5c005629afbb4983c879
Andrea Zagala <zagala.ac@obmontessori.edu.ph>
2026-02-24 13:48:38 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-
+function file() {
+    var file = document.getElementById("file").value;
+    file1 = Your + profile + picture + has + been + uploaded
+    window.alert(file1);
+}                          
